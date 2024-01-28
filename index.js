d023dbd969d8b27e67a3ef1e6a73424870665a8f
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-01-28 11:57:30 -0500

    Inputs in the response is useless as middleware since we don't know what the params are yet...

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   authAPIRequest: require("./src/apiAuth").authAPIRequest,
   gcpLogTransformer: require("./src/logging").gcpLogTransformer,
-  inputsInRes: require("./src/inputsInRes").inputsInRes,
   requestLogger: require("./src/logging").requestLogger,
   serverErrorHandler: require("./src/errors").serverErrorHandler,
 };
--- src/inputsInRes.js
+++
@@ -1,7 +0,0 @@
-const inputsInRes = (req, res, next) => {
-  const { body, query, params } = req;
-  res.locals.inputs = { body, query, params };
-  next();
-};
-
-module.exports = { inputsInRes };
