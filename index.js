df31fd6db2717de851b57eff02bd79a90fc4a0df
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-01-28 11:36:40 -0500

    Forgot to export inputsInRes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   authAPIRequest: require("./src/apiAuth").authAPIRequest,
   gcpLogTransformer: require("./src/logging").gcpLogTransformer,
+  inputsInRes: require("./src/inputsInRes").inputsInRes,
   requestLogger: require("./src/logging").requestLogger,
   serverErrorHandler: require("./src/errors").serverErrorHandler,
 };
